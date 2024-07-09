3de98bbca876bf053fa077cbfd00703dd6fd367a
Rajkumar Majhi <118106939+rajkumarmajhi31@users.noreply.github.com>
2024-07-10 02:30:35 +0530

    user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { createUser } = require("../controller/createUser");
 const { getUser } = require("../controller/getUsers");
+
+//add the controllers to the routes 
+
 router.post("/createUser", createUser);
 router.get("/getallUsers", getUser);
 
